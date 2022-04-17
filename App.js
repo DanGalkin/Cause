b73ec32044090b9d0361d0dcb2022f530fb3354e
dangalkin <dangalkin@gmail.com>
2022-04-17 22:32:19 +0300

    enable anonymous signin

--- App.js
+++ App.js
@@ -1,23 +1,75 @@
-// Cause app: 
+// Cause app
 
 import React from 'react';
 import {
   SafeAreaView,
   ScrollView,
   StyleSheet,
-  StatusBar,
   Text,
   View,
+  TouchableOpacity,
 } from 'react-native';
 
+import auth from '@react-native-firebase/auth';
+
 const App = () => {
+
+  const [initializing, setInitializing] = React.useState(true);
+  const [user, setUser] = React.useState();
+
+  const onAuthStateChanged = (user) => {
+    setUser(user);
+    if (initializing) setInitializing(false);
+  }
+
+  React.useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber;
+  }, []);
+
+  const logoutHandle = () => {
+    console.log('logout button pressed'); //for debug
+    auth()
+      .signOut()
+      .then(() => {console.log('Signed out')});
+  }
+
+  const loginHandle = () => {
+    console.log('login button pressed'); //for debug
+    auth()
+      .signInAnonymously()
+      .then(() => {console.log('user signed in anonimously')})
+      .catch(error => {
+        if(error.code === 'auth/operation-not-allowed') {
+          console.log('Enable anonymous in your firebase console');
+        }
+        console.error(error);
+      })
+  }
+
+  if (initializing) return null;
+
+  if (!user) { return (
+    <SafeAreaView style={{flex: 1}}>
+      <ScrollView contentInsetAdjustmentBehavior="automatic" contentContainerStyle={styles.centered}>
+        <View style={styles.centered}>
+          <Text>Please, sign-in:</Text>
+          <TouchableOpacity onPress={loginHandle}>
+            <Text style={{textDecorationLine: 'underline'}}>Log in</Text>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  ); }
 
   return (
     <SafeAreaView style={{flex: 1}}>
       <ScrollView contentInsetAdjustmentBehavior="automatic" contentContainerStyle={styles.centered}>
         <View style={styles.centered}>
-          <Text>Cause app:</Text>
-          <Text>Input regular datapoint about yourself or your environment and search for correlactions to find cause-effect</Text>
+          <Text>You are logged in as {user.uid}</Text>
+          <TouchableOpacity onPress={logoutHandle}>
+            <Text style={{textDecorationLine: 'underline'}}>Log out</Text>
+          </TouchableOpacity>
         </View>
       </ScrollView>
     </SafeAreaView>
