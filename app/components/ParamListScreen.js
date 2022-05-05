8651bb5dbfe33f519a4593311bb83b57723d44e2
dangalkin <dangalkin@gmail.com>
2022-05-05 23:49:32 +0300

    Show list of user params from a database on List Screen

--- app/components/ParamListScreen.js
+++ app/components/ParamListScreen.js
@@ -1,24 +1,81 @@
-import React from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import {
     Text,
     View,
     TouchableOpacity,
+    StyleSheet,
+    Button,
 } from 'react-native';
 
+import database from '@react-native-firebase/database';
+import { UserIdContext } from './Contexts.js';
+
 const ParamListScreen = ( {navigation} ) => {
+    const [paramList, setParamsList] = useState([]);
+
+    const userId = useContext(UserIdContext);
+
+    //fetch param data on load once
+    useEffect(() => {
+        database()
+            .ref(`/users/${userId}/params`)
+            .once('value')
+            .then(snapshot => {
+                setParamsList(snapshot.val());
+                });
+    }, []);
+
+    //subscribe for listening DB changes
+    useEffect(() => {
+        const onValueChange = database()
+          .ref(`/users/${userId}/params`)
+          .on('value', snapshot => {
+            setParamsList(snapshot.val());
+          });
+    
+        // Stop listening for updates when no longer required
+        return () => database().ref(`/users/${userId}/params`).off('value', onValueChange);
+      }, [userId]);
+    
     return(
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Here will be a list of params</Text>
-            <TouchableOpacity
-                onPress={() => navigation.navigate('EditParam')} >
-                <Text>Add new param</Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-                onPress={() => navigation.navigate('AddNote')} >
-                <Text>Add new note</Text>
-            </TouchableOpacity>
+        <View style={{ flex: 1 }}>
+            <Text style={styles.label}>Click on param to add new value:</Text>
+            <View style={styles.row}>
+                {Object.keys(paramList).map(key => (
+                    <TouchableOpacity
+                        key={key}
+                        style={styles.paramItem}
+                        onPress={() => navigation.navigate('AddNote')} >
+                        <Text>{paramList[key]['name']}</Text>
+                    </TouchableOpacity>
+                ))}
+            </View>
+            <View style={{ flex: 1, flexDirection: 'column-reverse', marginBottom: 30 }}>
+                <Button
+                    title='Add new param'
+                    onPress={() => navigation.navigate('EditParam')} />
+            </View>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    label: {
+        padding: 10,
+    },
+    paramItem: {
+        borderRadius: 4,
+        borderColor: 'orange',
+        borderWidth: 1,
+        padding: 5,
+        marginHorizontal: '1%',
+        marginVertical: 10,
+        flexDirection: 'row',
+    },
+    row: {
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+    }
+})
 
 export default ParamListScreen;
